5e5e93ffbf6d6d6aad4b77d33d640acf50b64910
saoatama <saoatama@gmail.com>
2019-05-31 23:57:18 +0000

    add 誤って消してしまったDBファイル

---
+++ db/migrate/20190531234953_create_restarurant.rb
@@ -0,0 +1,15 @@
+class Restarurant < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :title
+      t.integer :rating
+      t.integer :budget_day
+      t.integer :budget_night
+      t.text :address
+      t.integer :menu
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_220332) do
+ActiveRecord::Schema.define(version: 2019_05_31_235531) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,9 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_31_220332) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.integer "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "restaurants", force: :cascade do |t|
