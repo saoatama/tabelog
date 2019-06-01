23b30fea0fd62245833bbc07cab988ba45d2ed0d
saoatama <saoatama@gmail.com>
2019-06-01 21:08:17 +0000

    add budget_idとnameによる検索

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -4,6 +4,15 @@ class PublicController < ApplicationController
     end
     
     def list
+        @name = params[:name]
+        @budget_id = params[:budget_id]
+        if !@name then
+            @restaurants = Restaurant.where('budget_id = ?', params[:budget_id])
+        elsif !@budget_id then
+            @restaurants = Restaurant.where('name LIKE ?', "%#{params[:name]}%")
+        else
+            @restaurants = Restaurant.where('name LIKE ? AND budget_id = ?', "%#{params[:name]}%", params[:budget_id])
+        end
     end
     
     def list_categories
