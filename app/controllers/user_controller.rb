8adbc03c1b9e9b50ad80f5b65b1061178e0d0430
saoatama <saoatama@gmail.com>
2019-06-02 06:41:17 +0000

    fixed マイページで口コミを表示する

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,8 @@
 class UserController < ApplicationController
     before_action :authenticate_user!
     def my_page
-        unless current_user.id == params[:id].to_i then
+        @user = current_user
+        unless @user.id == params[:id].to_i then
             redirect_to root_url
         end
     end
