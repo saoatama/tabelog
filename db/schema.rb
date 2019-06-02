24542994dc1af737f6675b112c2ac2f87dfe0c48
saoatama <saoatama@gmail.com>
2019-06-02 07:53:52 +0000

    add 通報機能と削除機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_052623) do
+ActiveRecord::Schema.define(version: 2019_06_02_072240) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_06_02_052623) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "budget_id"
+    t.integer "reported"
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
